63f623966549c2f5e297fe12be4b58b6a2a2c41b
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-04-04 23:53:01 +0100

    Refactor: merge text under same context

--- test/SocialNetworkShould.js
+++ test/SocialNetworkShould.js
@@ -5,33 +5,29 @@ let User = require('../src/User.js');
 let PostAction = require('../src/Actions/PostAction.js');
 let ReadTimelineAction = require('../src/Actions/ReadTimelineAction.js');
 
-describe('Post Action Should', () => {
+describe('Social Networking Actions', () => {
     let userId = 'anyId';
     let userMessage = 'anyMessage';
     let user = User(userId);
     let userRepository = {
         getUser : (id) => user
     }
-    let postAction = PostAction(userRepository);
+    
+    describe('Post Action Should', () => {
+        let postAction = PostAction(userRepository);
 
-    it('let a user publish a message in his personal timeline', () => {
-        postAction.execute(userId, userMessage);
-        user.timeline()[0].should.be.equal(userMessage);
-    })
-})
+        it('let a user publish a message in his personal timeline', () => {
+            postAction.execute(userId, userMessage);
+            user.timeline()[0].should.be.equal(userMessage);
+        })
+    });
 
-describe('Read Action Should', () => {
-    let userId = 'anyId';
-    let userMessage = 'anyMessage';
-    let user = User(userId);
-    user.post(userMessage);
-    let userRepository = {
-        getUser : (id) => user
-    }
-    let readAction = ReadTimelineAction(userRepository);
+    describe('Read Action Should', () => {
+        let readAction = ReadTimelineAction(userRepository);
 
-    it('let a user read anothers timeline', () => {
-        let timeline = readAction.execute(userId);
-        timeline[0].should.be.equal(userMessage);
-    })
+        it('let a user read anothers timeline', () => {
+            let timeline = readAction.execute(userId);
+            timeline[0].should.be.equal(userMessage);
+        })
+    });
 })
