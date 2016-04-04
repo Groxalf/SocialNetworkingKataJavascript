e3cdb8d1c3c039c60fb8362938db327d6e1b1395
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-04-04 23:33:59 +0100

    Refactor: move declarations out of test case

--- test/SocialNetworkShould.js
+++ test/SocialNetworkShould.js
@@ -5,14 +5,15 @@ let User = require('../src/User.js');
 let PostAction = require('../src/Actions/PostAction.js');
 
 describe('Post Action Should', () => {
+    let userId = 'anyId';
+    let userMessage = 'anyMessage';
+    let user = User(userId);
+    let userRepository = {
+        getUser : (id) => user
+    }
+    let postAction = PostAction(userRepository);
+
     it('let a user publish a message in his personal timeline', () => {
-        let userId = 'anyId';
-        let userMessage = 'anyMessage';
-        let user = User(userId);
-        let userRepository = {
-            getUser : (id) => user
-        }
-        let postAction = PostAction(userRepository);
         postAction.execute(userId, userMessage);
         user.timeline[0].should.be.equal(userMessage);
     })
