020911de6f719c9af5bc7c59548f544cf4bf753d
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-04-04 23:45:49 +0100

    Read Action Should let a user read anothers timeline

--- test/SocialNetworkShould.js
+++ test/SocialNetworkShould.js
@@ -4,6 +4,20 @@ let should = require('chai').should();
 let User = require('../src/User.js');
 let PostAction = require('../src/Actions/PostAction.js');
 
+function ReadTimelineAction(userRepository) {
+    const _userRepository = userRepository;
+
+    function execute(userId) {
+        let user = userRepository.getUser(userId);
+        return user.timeline;
+    }
+
+    return {
+        execute
+    }
+}
+
+
 describe('Post Action Should', () => {
     let userId = 'anyId';
     let userMessage = 'anyMessage';
@@ -16,5 +30,21 @@ describe('Post Action Should', () => {
     it('let a user publish a message in his personal timeline', () => {
         postAction.execute(userId, userMessage);
         user.timeline[0].should.be.equal(userMessage);
+    })
+})
+
+describe('Read Action Should', () => {
+    let userId = 'anyId';
+    let userMessage = 'anyMessage';
+    let user = User(userId);
+    user.post(userMessage);
+    let userRepository = {
+        getUser : (id) => user
+    }
+    let readAction = ReadTimelineAction(userRepository);
+
+    it('let a user read anothers timeline', () => {
+        let timeline = readAction.execute(userId);
+        timeline[0].should.be.equal(userMessage);
     })
 })
